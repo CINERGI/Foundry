316b4db322cb39d85c3e0f542e6ff1dd6f7af6e5
Burak Ozyurt <bozyurt1@gmail.com>
2016-04-07 16:40:21 -0700

    changes

--- dependencies/install_prov_model_2mvn.sh
+++ dependencies/install_prov_model_2mvn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn install:install-file -Dfile=prov-model-0.5.1-SNAPSHOT.jar -DgroupId=org.openprovenance.prov -DartifactId=prov-model -Dversion=0.5.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=lib/prov-model-0.5.1-SNAPSHOT.jar -DgroupId=org.openprovenance.prov -DartifactId=prov-model -Dversion=0.5.1-SNAPSHOT -Dpackaging=jar
 
