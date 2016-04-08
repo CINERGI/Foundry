b4c7216ee2f20bfa6d546cb203783d3854ecb188
Burak Ozyurt <bozyurt1@gmail.com>
2016-04-07 17:02:54 -0700

    changes

--- dependencies/install_bnlp_model2mvn.sh
+++ dependencies/install_bnlp_model2mvn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn install:install-file -Dfile=$PWD/lib/bnlpkit-cinergi-models-0.2.jar -DgroupId=bnlp -DartifactId=bnlpkit-models -Dversion=0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=$PWD/lib/bnlpkit-cinergi-models-0.2.jar -DgroupId=bnlp -DartifactId=bnlpkit-cinergi-models -Dversion=0.2 -Dpackaging=jar
 
