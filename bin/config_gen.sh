9a7be4e7b6dbe14eebde636b97079e966d655bee
valentinedwv <david.valentine@gmail.com>
2019-03-09 13:55:53 -0800

    Faster Jackson, Security: CVE-2018-19360, CVE-2018-19361, CVE-2018-19362

--- bin/config_gen.sh
+++ bin/config_gen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn -q -f ../common/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.common.config.ConfigGenerator" -Dexec.args="$*"
+mvn -X -q -f ../common/pom.xml exec:java -Dexec.mainClass="org.neuinfo.foundry.common.config.ConfigGenerator" -Dexec.args="$*"
